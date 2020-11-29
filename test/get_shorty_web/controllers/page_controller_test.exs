4ed98df691d57986d4f9c250c91af81159f11aad
Mike Zornek <zorn@zornlabs.com>
2020-11-29 16:16:20 -0500

    Delete dead test file.

--- test/get_shorty_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GetShortyWeb.PageControllerTest do
-  use GetShortyWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
