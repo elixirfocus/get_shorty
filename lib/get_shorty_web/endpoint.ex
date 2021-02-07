70c49411cbd7d4cabc13200e1a9f9e19b2180470
Mike Zornek <zorn@zornlabs.com>
2021-02-07 09:18:48 -0500

    code for static asset link demo

--- lib/get_shorty_web/endpoint.ex
+++ lib/get_shorty_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule GetShortyWeb.Endpoint do
     at: "/",
     from: :get_shorty,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt reports)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
